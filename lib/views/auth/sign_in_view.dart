3d8d190831b045d04d8dd740befcdf9b35d413b4
Mampuru <mampuruntwampe@gmail.com>
2022-03-16 06:40:16 +0200

    Added sign up view

--- lib/views/auth/sign_in_view.dart
+++ lib/views/auth/sign_in_view.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:mythanx/views/auth/sign_up_view.dart';
 import 'package:mythanx/views/widgets/primary_button.dart';
 import 'package:mythanx/views/widgets/primary_textfield.dart';
 
 import '../../constants.dart';
+import '../home_view.dart';
 
 class SignInView extends StatefulWidget {
   const SignInView({Key key}) : super(key: key);
@@ -12,59 +14,63 @@ class SignInView extends StatefulWidget {
 }
 
 class _SignInViewState extends State<SignInView> {
-  final emailController = TextEditingController();
+  final userController = TextEditingController();
   final passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Image.asset(logoPath,width: 300,),
-          const SizedBox(height: 40.0,),
-          const Text("Sign In",style: TextStyle(fontSize: 25.0,fontWeight: FontWeight.bold),),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: PrimaryTextfield(controller: emailController, label: "Email"),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: PrimaryTextfield(controller: passwordController, label: "Password"),
-              ),
-              InkWell(
-                onTap: () async {
-                  //forgot password endpoint
-                },
-                  child: const Padding(
-                    padding: EdgeInsets.fromLTRB(200,16,0,16),
-                    child: Text("Forgot Password ?",
-                      style:TextStyle(color: kTertiaryColor,fontWeight: FontWeight.bold),),
-                  )),
-              const SizedBox(height: 30.0,),
-              PrimaryButton(buttonName: "Sign In", onTap: () async {
-              }),
-              const SizedBox(height: 50.0,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text("Don't have an account ? ",),
-                  InkWell(
-                    onTap: () async {
-                      //redirect
-                    },
-                    child: const Text("Sign Up",
-                      style:TextStyle(color: kTertiaryColor,fontWeight: FontWeight.w900),),
-                  ),
-                ],
-              )
-            ],
-          ),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: 100.0,),
+            Image.asset(logoPath,width: 300,),
+            const SizedBox(height: 40.0,),
+            const Text("Sign In",style: TextStyle(fontSize: 25.0,fontWeight: FontWeight.bold),),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: PrimaryTextfield(controller: userController, label: "Username"),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: PrimaryTextfield(controller: passwordController, label: "Password"),
+                ),
+                InkWell(
+                  onTap: () async {
+                    //forgot password endpoint
+                  },
+                    child: const Padding(
+                      padding: EdgeInsets.fromLTRB(200,16,0,16),
+                      child: Text("Forgot Password ?",
+                        style:TextStyle(color: kTertiaryColor,fontWeight: FontWeight.bold),),
+                    )),
+                const SizedBox(height: 30.0,),
+                PrimaryButton(buttonName: "Sign In", onTap: () async {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => const HomeView()));
+                }),
+                const SizedBox(height: 50.0,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text("Don't have an account ? ",),
+                    InkWell(
+                      onTap: () async {
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => const SignUpView()));
+                      },
+                      child: const Text("Sign Up",
+                        style:TextStyle(color: kTertiaryColor,fontWeight: FontWeight.w900),),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/views/auth/sign_up_view.dart
+++ lib/views/auth/sign_up_view.dart
@@ -1,4 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:mythanx/views/auth/sign_in_view.dart';
+import 'package:mythanx/views/widgets/primary_button.dart';
+import 'package:mythanx/views/widgets/primary_textfield.dart';
+
+import '../../constants.dart';
+import '../home_view.dart';
 
 class SignUpView extends StatefulWidget {
   const SignUpView({Key key}) : super(key: key);
@@ -8,9 +15,79 @@ class SignUpView extends StatefulWidget {
 }
 
 class _SignUpViewState extends State<SignUpView> {
+  final userController = TextEditingController();
+  final passwordController = TextEditingController();
+  final nameController = TextEditingController();
+  final passwordConfirmController = TextEditingController();
+  final phoneController = TextEditingController();
+  final countryController = TextEditingController();
+  final emailController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: 40.0,),
+            const Text("Sign Up",style: TextStyle(fontSize: 25.0,fontWeight: FontWeight.bold),),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(16,8,16,8),
+                  child: PrimaryTextfield(controller: nameController, label: "Name"),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(16,8,16,8),
+                  child: PrimaryTextfield(controller: userController, label: "Username"),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(16,8,16,8),
+                  child: PrimaryTextfield(controller: emailController, label: "Email"),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(16,8,16,8),
+                  child: PrimaryTextfield(controller: phoneController, label: "Phone",textInputType: TextInputType.number,),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(16,8,16,8),
+                  child: PrimaryTextfield(controller: countryController, label: "Country"),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(16,8,16,8),
+                  child: PrimaryTextfield(controller: passwordController, label: "Password"),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(16,8,16,8),
+                  child: PrimaryTextfield(controller: passwordConfirmController, label: "Confirm Password"),
+                ),
+                const SizedBox(height: 30.0,),
+                PrimaryButton(buttonName: "Sign Up", onTap: () async {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => const HomeView()));
+                }),
+                const SizedBox(height: 40.0,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text("Have an account already? ",),
+                    InkWell(
+                      onTap: () async {
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => const SignInView()));
+                      },
+                      child: const Text("Sign In",
+                        style:TextStyle(color: kTertiaryColor,fontWeight: FontWeight.w900),),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10.0,),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
