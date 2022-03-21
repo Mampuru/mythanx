f25f551c6937939a873a946f510ee50347862f49
Mampuru <mampuruntwampe@gmail.com>
2022-03-21 04:25:29 +0200

    Added the endpoints to API

---
+++ lib/api/api_config.dart
@@ -0,0 +1,6 @@
+
+String baseUrl = "http://api.mythanx.xyz";
+
+String signUp = baseUrl + "/auth/signup";
+String signIn = baseUrl + "/auth/login";
+String getUser = baseUrl + "/users/me";
---
+++ lib/api/api_service.dart
@@ -0,0 +1,81 @@
+
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:mythanx/data/dto/user_dto.dart';
+import 'package:mythanx/data/mapper/user_model.dart';
+import 'package:http/http.dart' as http;
+import 'package:mythanx/data/mapper/user_profile_model.dart';
+import 'package:mythanx/views/widgets/primary_toast.dart';
+
+import '../constants.dart';
+import 'api_config.dart';
+
+Future<User> register(UserDto user) async {
+  try {
+    final http.Response response = await http.post(
+      Uri.parse(signUp),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8'
+      },
+      body: userDtoToJson(user)
+    );
+
+    if (response.statusCode == 201) {
+       return userFromJson(response.body);
+    } else if (response.statusCode == 401) {
+      PrimaryToast().displayToast("Unauthorized", kErrorColor);
+      throw Exception('Unauthorized Access: Failed to send location');
+    }
+  } on SocketException {
+    PrimaryToast().displayToast("No Internet connection", kInfoColor);
+    throw Exception('No Internet connection');
+  }
+  return null;
+}
+
+Future<User> login(String username,password) async {
+  try {
+    final http.Response response = await http.post(
+        Uri.parse(signUp),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8'
+        },
+        body: jsonEncode(
+            <String, String>{'username': username, 'password': password})
+        );
+
+        if (response.statusCode == 201) {
+          return userFromJson(response.body);
+    } else if (response.statusCode == 401) {
+  PrimaryToast().displayToast("Unauthorized", kErrorColor);
+  throw Exception('Unauthorized Access: Failed to send location');
+  }
+  } on SocketException {
+  PrimaryToast().displayToast("No Internet connection", kInfoColor);
+  throw Exception('No Internet connection');
+  }
+  return null;
+}
+
+Future<UserProfile> getUser(String apiKey) async {
+  try {
+    final http.Response response = await http.get(
+        Uri.parse(signUp),
+        headers: <String, String>{
+          'Authorization': apiKey,
+          'Content-Type': 'application/json; charset=UTF-8'
+        });
+
+        if (response.statusCode == 201) {
+          return userProfileFromJson(response.body);
+    } else if (response.statusCode == 401) {
+  PrimaryToast().displayToast("Unauthorized", kErrorColor);
+  throw Exception('Unauthorized Access: Failed to send location');
+  }
+  } on SocketException {
+  PrimaryToast().displayToast("No Internet connection", kInfoColor);
+  throw Exception('No Internet connection');
+  }
+  return null;
+}
