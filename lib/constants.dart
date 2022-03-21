48d9945f10d2d0ccf84045278fa1fa5af9ba2360
Mampuru <mampuruntwampe@gmail.com>
2022-03-21 04:21:37 +0200

    Added the logger lib for debugging

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
+
+//For debugging purpose
+Logger logger = Logger();
 
 // Colour
 const kPrimaryColor = Color(0xFF1f559a);
@@ -7,6 +11,11 @@ const kTertiaryColor = Color(0xFF070D59);
 const kCharcoal = Color(0xFF21212C);
 const kWhite = Color(0xFFFFFFFF);
 const kBodyTextColor = Color(0xFF666666);
+
+const kErrorColor = Color(0xFFE02416);
+const kSuccessColor = Color(0xFF00A000);
+const kInfoColor = Color(0xFF0099CC);
+const kWarningColor = Color(0xFFFFB114);
 
 //Image
 const logoPath = "assets/icons/mythanx1024.png";
