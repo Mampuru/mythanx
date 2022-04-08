6d053830952fe611bb9782468df36c8252515792
Mampuru <mampuruntwampe@gmail.com>
2022-04-09 00:54:34 +0200

    Updated the config file http -> https

--- lib/api/api_config.dart
+++ lib/api/api_config.dart
@@ -1,6 +1,7 @@
 
-String baseUrl = "http://api.mythanx.xyz";
+String baseUrl = "https://api.mythanx.xyz";
 
 String signUp = baseUrl + "/auth/signup";
 String signIn = baseUrl + "/auth/login";
 String getUser = baseUrl + "/users/me";
+String getCountry  = baseUrl + "/data/countries";
